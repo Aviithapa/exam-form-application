2c32f3d996c9e1bef10597596333ff19a78d106d
Abhishek Thapa <abhishek.thapa@nextly.solutions>
2024-03-06 16:16:55 +0545

    update format

--- resources/views/admin/pages/admin/applicant-format.blade.php
+++ resources/views/admin/pages/admin/applicant-format.blade.php
@@ -60,10 +60,7 @@
 <spam style="font-size: 12px;">परीक्षा नं  ३१/ तहः अधिवक्ता। </spam> <br/>
 
 <spam style="font-size: 12px;">परीक्षा नं्‍‍‍ ३१ भनेकाे ठाउँमा Exam Creat गर्दाकाे auto आउने बनाउने । </spam>
-                                    अनुसूची १ <br/>
                                    
-                                   नेपाल कानून व्यवसायी परिषद <br/>
-                                  आवेदन फारम  </h3>
                                   </div>
                                <div class="col-sm-8" style="text-align: center; padding-left: 100px;">
                                 
